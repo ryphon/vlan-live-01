98ad7f234dfbbb1dc41baa8f8830826b9181cc64
Dylan Shepard <dylan@shepard.dev>
2021-02-26 13:19:48 -0700

    fixing lifecycle action token

--- prime/us-west-2/prod/vlan/valheim/termination.py
+++ prime/us-west-2/prod/vlan/valheim/termination.py
@@ -13,6 +13,14 @@ docker_client = docker.from_env()
 backup_queue_url = 'https://sqs.{}.amazonaws.com/456410706824/valheim-default-lifecycle'.format(region)
 # frustrating to hard code this, idk about this quite yet
 queue_url = os.environ.get('SQS_QUEUE_URL', backup_queue_url)
+'''
+{
+    'MessageId': '34c9da53-fbb5-4823-8679-b07297030896',
+    'ReceiptHandle': 'AQEBP5yRsqjtQt5cLYBry/2DxGh9jFBjUiDuiLMXK/NMdewHSluWWvVTMIpuNCkz2k7xwd6VI5oQPg+JsMBVPFKtKtpVKgCA/aJNckAWjxOS4LN+FxDJFvfMpjtMLjtIRugFMiGSE4AMCLduJwX8DMpX0XpPuMx5gDtgsSDh2dQmiR1CSvndyF2yg+K5EP3guSQP35Nczzc/jhWHbmXIAj19igenkXLhB7v7lML79CZzwZTiKrOt8TwnBOcuO7SZD78MY9gGFQgSny8oTY7F6WRsWQRPbH1OurQtvI5rY2RlVWoRcXaXXVIdUoLYwqRySultgJ4BLsmDASmHuwaIjZ2J0FNvQmjkezQDCEZxGUL/Vz/tc9PBi+JNKtg04IEZ6PWCmqOREuodNd+yKxEVi7RKkuJfhJU6QATZwaet8Fd6FW8=',
+    'MD5OfBody': 'd27dac4b7d3f761b5bd3d32d5d5e81fb',
+    'Body': '{"LifecycleHookName":"valheim-default-terminate","AccountId":"456410706824","RequestId":"a7b2d01b-69d3-4940-910a-3a2ee0cbe1f9","LifecycleTransition":"autoscaling:EC2_INSTANCE_TERMINATING","AutoScalingGroupName":"valheim-default","Service":"AWS Auto Scaling","Time":"2021-02-26T06:40:06.663Z","EC2InstanceId":"i-016a0e69d8b5ae49d","NotificationMetadata":"{\\n  \\"game\\": valheim,\\n  \\"gameType\\": default,\\n  \\"asgName\\": valheim-default\\n}\\n","LifecycleActionToken":"dce60e5a-ffe3-4a47-bd57-56effbaefd60"}',
+    'Attributes': {'SentTimestamp': '1614321606685'}}
+'''
 while True:
     try:
         print('Rx')
@@ -32,10 +40,9 @@ while True:
         message = response['Messages'][0]
         print('Message recieved, starting kill process now.')
         message_body = json.loads(message['Body'])
-
         receipt_handle = message['ReceiptHandle']
+        lifecycleToken = message_body['LifecycleActionToken']
         metadata = json.loads(message_body['NotificationMetadata'].strip())
-
         lifecycle_hook_name = message_body['LifecycleHookName']
         autoscaling_group_name = metadata['asgName']
 
@@ -56,11 +63,11 @@ while True:
 
         print('Complete Lifecycle now.')
         # now you can allow the instance to die
-        asg.complete_lifecycle_action(
+        resp = asg.complete_lifecycle_action(
             LifecycleHookName=lifecycle_hook_name,
             AutoScalingGroupName=autoscaling_group_name,
             LifecycleActionResult='COMPLETE',
-            LifecycleActionToken=message['LifecycleActionToken']
+            LifecycleActionToken=lifecycleToken
         )
 
         # ur gonna die anyway LUL
