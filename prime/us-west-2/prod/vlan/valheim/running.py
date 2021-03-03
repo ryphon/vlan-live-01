2a908d7ac87693b28c31f87c54d38a9b5697ab8a
Dylan Shepard <dylan@shepard.dev>
2021-03-03 11:18:16 -0700

    running script works basically

--- prime/us-west-2/prod/vlan/valheim/running.py
+++ prime/us-west-2/prod/vlan/valheim/running.py
@@ -19,7 +19,7 @@ def main():
     parser.add_argument('--gameType', type=str)
     args = parser.parse_args()
 
-    SERVER_ADDRESS = "{}:{}".format(args.server_address, args.server_port)
+    SERVER_ADDRESS = (args.serverAddress, args.serverPort)
     # server.info().values = {'response_type': 73, 'protocol': 17, 'server_name': "Garry's Mod", 'map': 'ttt_dolls', 'folder': 'garrysmod', 'game': 'TTT2 (Advanced Update)', 'app_id': 4000, 'player_count': 0, 'max_players': 16, 'bot_count': 0, 'server_type': <ServerType 100 'Dedicated'>, 'platform': <Platform 108 'Linux'>, 'password_protected': 0, 'vac_enabled': 1, 'version': '2020.03.17'}
     ssm = boto3.client('ssm', region_name=region)
     cert = json.loads(ssm.get_parameter(Name='firebase_secrets', WithDecryption=True)['Parameter']['Value'])
@@ -34,13 +34,14 @@ def main():
     while True:
         try:
             with valve.source.a2s.ServerQuerier(SERVER_ADDRESS) as server:
-                info = server.info().values
-                print(info)
-                del info['platform']
-                del info['server_type']
-                del info['bot_count']
-                print('del')
-                print(info)
+                server.info()
+                # need to genericize this
+                store.document(f'games/{args.game}').set({
+                    f'{args.gameType}': {
+                        'started': True,
+                        'ready': True
+                    }
+                }, merge=True)
                 break
         except NoResponseError:
             print('No response yet!')
